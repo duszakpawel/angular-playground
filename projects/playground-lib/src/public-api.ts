74b69f280e1c76922cd79c221d0e05ad87287974
Pablo <duszakpawel@gmail.com>
2024-01-22 17:05:00 +0100

    Add playground-lib.module, playground-lib.service

---
+++ projects/playground-lib/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of playground-lib
+ */
+
+export * from './lib/playground-lib.service';
+export * from './lib/playground-lib.component';
+export * from './lib/playground-lib.module';
+export * from './lib/logo/logo.component';
