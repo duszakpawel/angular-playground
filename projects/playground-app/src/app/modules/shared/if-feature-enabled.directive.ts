ce57d2e519abd78c60b3dc9e7ecc0501579ae5ae
Pablo <duszakpawel@gmail.com>
2024-02-07 17:12:00 +0100

    Add if-feature-enabled.directive

---
+++ projects/playground-app/src/app/modules/shared/if-feature-enabled.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, Inject, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { AppConfig } from 'playground-api/types';
+import { APP_CONFIG } from '../../app-config-token';
+
+@Directive({
+  selector: '[appIfFeatureEnabled]'
+})
+export class IfFeatureEnabledDirective {
+  private hasView = false;
+
+  @Input('appIfFeatureEnabled') set featureName(value: keyof AppConfig['features']) {
+    if (this.appConfig.features[value] && !this.hasView) {
+      this.viewContainer.createEmbeddedView(this.templateRef);
+      this.hasView = true;
+    } else {
+      this.viewContainer.clear();
+      this.hasView = false;
+    }
+  }
+
+  constructor(private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef,
+    @Inject(APP_CONFIG) private appConfig: AppConfig) { }
+
+}
