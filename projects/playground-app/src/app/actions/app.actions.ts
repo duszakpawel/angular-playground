3a7ee7addc4ceff7a3d4d0e540ab1c53e0461174
Pablo <duszakpawel@gmail.com>
2024-02-29 17:12:00 +0100

    Add app.actions

---
+++ projects/playground-app/src/app/actions/app.actions.ts
@@ -0,0 +1,8 @@
+import { createAction, props } from '@ngrx/store';
+
+export const showNavigation = createAction('[App] Show Navigation');
+export const hideNavigation = createAction('[App] Hide Navigation');
+
+
+
+
