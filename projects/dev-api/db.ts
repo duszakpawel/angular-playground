b59f70353018cc9c7a99f972ea15c64b90cd23e7
Pablo <duszakpawel@gmail.com>
2024-03-09 17:12:00 +0100

    Add playground-api mocks

---
+++ projects/dev-api/db.ts
@@ -0,0 +1,7 @@
+import { mockedLabs, mockedCats, mockedSubscriptions } from '../playground-api/mocks';
+
+export const db = {
+  labs: mockedLabs,
+  cats: mockedCats,
+  subscriptions: mockedSubscriptions,
+};
