32edc3a1cb8a1bb51b51582b12d10233b5c61738
Pablo <duszakpawel@gmail.com>
2024-03-10 17:12:00 +0100

    Add playground-api main.js, tsconfig.json, index.ts

---
+++ main.js
@@ -0,0 +1,47 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  const startUrl = url.format({
+    pathname: path.join(__dirname, 'dist/playground-app/index.html'),
+    protocol: 'file:',
+    slashes: true
+  });
+
+  mainWindow.loadURL(startUrl);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  setTimeout(() => {
+    const mainWindow = BrowserWindow.getFocusedWindow();
+    if (mainWindow) {
+      mainWindow.webContents.reload();
+    }
+  }, 1000);
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
---
+++ projects/playground-api/services/index.ts
@@ -0,0 +1,4 @@
+export * from './app-config/app-config.service';
+export * from './labs/labs.service';
+export * from './cats/cats.service';
+export * from './moons/moons.service';
